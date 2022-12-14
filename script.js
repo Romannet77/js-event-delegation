ffa485a1d0acb0cde79734f4f75bf072de23f583
Romannet77 <yurchenkor33@gmail.com>
2022-12-15 01:02:57 +0200

    adding my project to git

---
+++ script.js
@@ -0,0 +1,62 @@
+const button = document.querySelector("button");
+
+const input = document.querySelector("#input-product");
+
+const ul = document.querySelector("ul");
+
+const h2 = document.querySelector("h2");
+
+const h4 = document.querySelector("h4");
+
+const mainCode = document.querySelector(".event-delegation");
+
+const deleteButtons = document.querySelectorAll(".btn-delete");
+
+/*Add an element at the beginning of the list by clicking "Add Framework" button*/
+
+button.addEventListener("click", function () {
+  if (input.value.length > 0) {
+    const htmlEl = `<li class = "element-list">${input.value}<span>framework</span><button class="btn-delete">delete</button></li>`;
+    ul.insertAdjacentHTML("afterBegin", htmlEl);
+
+    input.value = "";
+  }
+});
+
+/*Add an element at the beginning of the list by pressing key "Enter"*/
+
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter" && input.value !== "") {
+    const htmlEl = `<li class = "element-list">${input.value}<span>framework</span><button class="btn-delete">delete</button></li>`;
+    ul.insertAdjacentHTML("afterBegin", htmlEl);
+
+    input.value = "";
+  }
+});
+
+/* Adding handling of the event not directly to the button "delete" element but to the "ul" element standing above it in DOM structure. So, We are catching an event triggered in our parent ("ul") element that matches our selected child (button "delete") element */
+
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-delete")) {
+    e.target.parentElement.remove();
+    h2.style.color = "red";
+    h4.classList.toggle("h4-toogle-style");
+    mainCode.classList.toggle("event-delegation");
+  }
+});
+
+/*the ordinary code will affect only already existing elements but the Dynamically Created Elements will not be affected:
+
+deleteButtons.forEach((delBtn) => {
+  delBtn.addEventListener("click", () => {
+    delBtn.parentElement.remove();
+  });
+});*/
+
+/*Toggles the .done class by clicking on the name of programming language or framework */
+
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("element-list")) {
+    e.target.classList.toggle("done");
+  }
+});
